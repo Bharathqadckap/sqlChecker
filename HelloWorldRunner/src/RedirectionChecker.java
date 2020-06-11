20deccbc62c41313b0e642efabf0f9a2814786e1
BharathManickam <bharathm@dckap.com>
2020-06-11 10:18:40 +0530

    test

--- HelloWorldRunner/src/RedirectionChecker.java
+++ HelloWorldRunner/src/RedirectionChecker.java
@@ -22,6 +22,7 @@ public class RedirectionChecker {
 		//added another commenta
 		//added another comment
 		//added another comment
+		//added another comment
 		propy = new Properties();
 		InputStream propertyStream = RedirectionChecker.class.getResourceAsStream("application.properties");
 		propy.load(propertyStream);
