90966367c17c768712e43596e0a6f4318eec5574
BharathManickam <bharathm@dckap.com>
2020-06-11 07:07:48 +0530

    testing

--- HelloWorldRunner/src/RedirectionChecker.java
+++ HelloWorldRunner/src/RedirectionChecker.java
@@ -21,6 +21,7 @@ public class RedirectionChecker {
 		HtmlUnitDriver driver = new HtmlUnitDriver();
 		//added another commenta
 		//added another comment
+		//added another comment
 		propy = new Properties();
 		InputStream propertyStream = RedirectionChecker.class.getResourceAsStream("application.properties");
 		propy.load(propertyStream);
