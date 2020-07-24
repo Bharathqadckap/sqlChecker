5421afb00bf9f5d80ef6b772df49fd2ba5b86802
BharathManickam <bharathm@dckap.com>
2020-07-24 09:51:56 +0530

    Just added a comment

--- HelloWorldRunner/src/RedirectionChecker.java
+++ HelloWorldRunner/src/RedirectionChecker.java
@@ -20,6 +20,7 @@ public class RedirectionChecker {
 
 		HtmlUnitDriver driver = new HtmlUnitDriver();
 		//added a new comment
+		//added a new comment
 		InputStream propertyStream = RedirectionChecker.class.getResourceAsStream("application.properties");
 		propy.load(propertyStream);
 		// assign property values to local variables
